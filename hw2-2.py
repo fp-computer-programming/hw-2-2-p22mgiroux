c71c4dcb119ae8f041feef180a3893f5bc1b8574
p22mgiroux <p22mgiroux@fairfieldprep.org>
2021-09-23 10:35:56 -0400

    Fix var naming convention

--- hw2-2.py
+++ hw2-2.py
@@ -1,11 +1,8 @@
 # Author: MOG 9/16/21
 
-free_Throws = 1
-two_Pts = 4
-three_Pts = 7
-total_Pts = (free_Throws) + (two_Pts * 2) + (three_Pts * 3)
+free_throws = 1
+two_pts = 4
+three_pts = 7
+total_pts = (free_throws) + (two_pts * 2) + (three_pts * 3)
 
-s = ""
-if(total_Pts != 1): s = "s"
-
-print("The player scored {} point{} in the game.".format(total_Pts, s))
+print("Total Pts: " + str(total_pts))
